7017afbcbd4474e259906d9544c2dc8e495c9940
Zhizhen Zhong <zhizhenz@mit.edu>
2022-05-30 19:52:31 +0000

    modify pruning.py

--- pruning.py
+++ pruning.py
@@ -51,43 +51,42 @@ else:
     print('Not using CUDA!!!')
 
 # Loader
-kwargs = {'num_workers': 5, 'pin_memory': True} if use_cuda else {}
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('data', train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('data', train=False, transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])),
-    batch_size=args.test_batch_size, shuffle=False, **kwargs)
-
+# kwargs = {'num_workers': 5, 'pin_memory': True} if use_cuda else {}
 # train_loader = torch.utils.data.DataLoader(
-#     datasets.CIFAR10('data', train=True, download=True, transform=transforms.Compose([
-#                        transforms.RandomCrop(32, padding=4),
-#                        transforms.RandomHorizontalFlip(),
+#     datasets.MNIST('data', train=True, download=True,
+#                    transform=transforms.Compose([
 #                        transforms.ToTensor(),
-#                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-#                     ])),
+#                        transforms.Normalize((0.1307,), (0.3081,))
+#                    ])),
 #     batch_size=args.batch_size, shuffle=True, **kwargs)
-
 # test_loader = torch.utils.data.DataLoader(
-#     datasets.CIFAR10('data', train=False, transform=transforms.Compose([
-#                        transforms.RandomCrop(32, padding=4),
-#                        transforms.RandomHorizontalFlip(),
+#     datasets.MNIST('data', train=False, transform=transforms.Compose([
 #                        transforms.ToTensor(),
-#                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-#                     ])),
+#                        transforms.Normalize((0.1307,), (0.3081,))
+#                    ])),
 #     batch_size=args.test_batch_size, shuffle=False, **kwargs)
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10('data', train=True, download=True, transform=transforms.Compose([
+                       transforms.RandomCrop(32, padding=4),
+                       transforms.RandomHorizontalFlip(),
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                    ])),
+    batch_size=args.batch_size, shuffle=True, **kwargs)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.CIFAR10('data', train=False, transform=transforms.Compose([
+                       transforms.RandomCrop(32, padding=4),
+                       transforms.RandomHorizontalFlip(),
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+                    ])),
+    batch_size=args.test_batch_size, shuffle=False, **kwargs)
 
 
 # Define which model to use
 model = LeNet(mask=True).to(device)
-# model = LeNet_5(mask=True).to(device)
 # model = models.resnet18(pretrained=False).to(device)
 
 print(model)
